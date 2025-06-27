87d4ad90d7f40badd0f0f5524be307c02bc36068
gus <gus@needhamsoftware.com>
2025-06-26 21:47:27 -0400

    Half of a solution for issue #26 - still haven't Figured out how to add the type information to JSOGRef as shown in Issue26Test.java

--- src/main/java/com/voodoodyne/jackson/jsog/JSOGRefDeserializer.java
+++ src/main/java/com/voodoodyne/jackson/jsog/JSOGRefDeserializer.java
@@ -1,12 +1,13 @@
 package com.voodoodyne.jackson.jsog;
 
 
+import java.io.IOException;
 import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.JsonToken;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.databind.JsonNode;
-import java.io.IOException;
+import com.fasterxml.jackson.databind.jsontype.TypeDeserializer;
 
 /**
  * Knows how to take either form of a JSOGRef (string or {@ref:string} and convert it back into a JSOGRef.
@@ -16,7 +17,7 @@ import java.io.IOException;
 public class JSOGRefDeserializer extends JsonDeserializer<JSOGRef>
 {
 	@Override
-	public JSOGRef deserialize(JsonParser jp, DeserializationContext ctx) throws IOException, JsonProcessingException {
+	public JSOGRef deserialize(JsonParser jp, DeserializationContext ctx) throws IOException {
 		JsonNode node = ctx.readValue(jp, JsonNode.class);
 		if (node.isTextual()) {
 			return new JSOGRef(node.asText());
@@ -25,4 +26,27 @@ public class JSOGRefDeserializer extends JsonDeserializer<JSOGRef>
 		}
 	}
 
+	@Override
+	public Object deserializeWithType(JsonParser p, DeserializationContext ctxt, TypeDeserializer typeDeserializer) throws IOException {
+		// in the event of default typing, this gets called instead of deserialize above, and it seems to get
+		// called for each @id (the first case) and for each {"@ref":"#"} (the second case)
+		if (p.currentToken() == JsonToken.VALUE_STRING) {
+			return new JSOGRef(p.getText());
+		}
+		if (p.currentToken() == JsonToken.FIELD_NAME) {
+			// we are being called for { "@ref":"#" }
+			//   and starting here ------^
+			p.nextToken();
+			if (p.currentToken() != JsonToken.VALUE_STRING) {
+				throw new IllegalStateException("@ref attribute should be followed by a value?");
+			}
+		String text = p.getText(); //grab our id
+		p.nextToken(); // consume END_OBJECT
+		return new JSOGRef(text);
+		} else {
+			throw new IllegalStateException("Unexpected Token Type:" + p.currentToken().name());
+		}
+	}
 }
+
+
---
+++ src/test/java/com/voodoodyne/jackson/jsog/Issue26Test.java
@@ -0,0 +1,88 @@
+package com.voodoodyne.jackson.jsog;
+
+import java.util.ArrayList;
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.jsontype.BasicPolymorphicTypeValidator;
+import com.fasterxml.jackson.databind.jsontype.PolymorphicTypeValidator;
+import org.junit.Test;
+
+public class Issue26Test {
+
+  // @formatter:off
+  String TEST_JSON="[\"java.util.ArrayList\",[" +
+
+      "{" +
+        "\"@class\":\"com.voodoodyne.jackson.jsog.Issue26Test$Outer\"," +
+        "\"@id\":\"1\"," +
+        "\"inner\":{" +
+          "\"@class\":\"com.voodoodyne.jackson.jsog.Issue26Test$Inner\"," +
+          "\"@id\":\"2\"," +
+          "\"outer\":{" +
+            "\"@class\":\"com.voodoodyne.jackson.jsog.Issue26Test$Outer\"," + // <-- added for issue 26
+            "\"@ref\":\"1\"" +
+          "}" +
+        "}" +
+      "}," +
+
+      "{\"" +
+        "@class\":\"com.voodoodyne.jackson.jsog.Issue26Test$Inner\"," + // <-- added for issue 26
+        "\"@ref\":\"2\"}" +
+      "]]";
+
+  // @formatter:on
+  @Test
+  public void testDeserializeWithDefaultTyping() throws JsonProcessingException {
+    PolymorphicTypeValidator ptv = BasicPolymorphicTypeValidator.builder().allowIfSubType(Object.class).build();
+    ObjectMapper mapper = new ObjectMapper()
+        .activateDefaultTyping(ptv, ObjectMapper.DefaultTyping.EVERYTHING,
+            JsonTypeInfo.As.PROPERTY);
+    // also test this when jackson is upgraded...
+    // List<Object> target = mapper.readerForListOf(Object.class).readValue(json);
+    mapper.readValue(TEST_JSON, ArrayList.class);
+
+  }
+
+  // classes used in this test
+
+  @SuppressWarnings("unused")
+  @JsonIdentityInfo(generator = JSOGGenerator.class)
+  public static class Outer {
+    private Inner inner;
+
+    public Inner getInner() {
+      return inner;
+    }
+
+    public void setInner(Inner inner) {
+      this.inner = inner;
+    }
+  }
+
+  @SuppressWarnings("unused")
+  @JsonIdentityInfo(generator = JSOGGenerator.class)
+  public static class Inner {
+
+    private Outer outer;
+
+    Inner() {
+    }
+
+    public Inner(Outer outer) {
+      this.outer = outer;
+      outer.inner = this;
+    }
+
+    public Outer getOuter() {
+      return outer;
+    }
+
+    public void setOuter(Outer outer) {
+      this.outer = outer;
+    }
+  }
+
+
+}
