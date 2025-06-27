4afdd3af6ddbea54c373f9da83113ffeeb162d5f
gus <gus@needhamsoftware.com>
2025-06-27 11:54:47 -0400

    failing serialization test for default typing

--- src/test/java/com/voodoodyne/jackson/jsog/Issue26Test.java
+++ src/test/java/com/voodoodyne/jackson/jsog/Issue26Test.java
@@ -1,6 +1,9 @@
 package com.voodoodyne.jackson.jsog;
 
+import static org.junit.Assert.assertEquals;
+
 import java.util.ArrayList;
+import java.util.List;
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -8,6 +11,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.jsontype.BasicPolymorphicTypeValidator;
 import com.fasterxml.jackson.databind.jsontype.PolymorphicTypeValidator;
 import org.junit.Test;
+import org.testng.collections.Lists;
 
 public class Issue26Test {
 
@@ -43,6 +47,23 @@ public class Issue26Test {
     // List<Object> target = mapper.readerForListOf(Object.class).readValue(json);
     mapper.readValue(TEST_JSON, ArrayList.class);
 
+  }
+
+  @Test
+  public void testSerializeWithDefaultTypeing() throws JsonProcessingException {
+    Outer outer = new Outer();
+    Inner inner = new Inner(outer);
+    // Turn on type info
+    PolymorphicTypeValidator ptv = BasicPolymorphicTypeValidator.builder().allowIfSubType(Object.class).build();
+    ObjectMapper mapper = new ObjectMapper()
+        .activateDefaultTyping(ptv, ObjectMapper.DefaultTyping.EVERYTHING,
+            JsonTypeInfo.As.PROPERTY);
+
+
+    List<Object> source = Lists.newArrayList(outer, inner);
+    String json = mapper.writeValueAsString(source);
+
+    assertEquals(TEST_JSON,json);
   }
 
   // classes used in this test
